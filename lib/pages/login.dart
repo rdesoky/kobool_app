3ae2fa0b836c491bc2bbd3a1f51b79f7c4b8a470
Ramy Eldesoky <rdesoky@gmail.com>
2025-12-23 21:22:10 -0800

    refactor: update Login widget to use hooks and add login/logout functionality chore: add flutter_hooks dependency

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class Login extends StatelessWidget {
+class Login extends HookWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var isLogin = useState(false);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Login'), centerTitle: false),
 
       body: Center(
         child: Column(
+          spacing: 16,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("Login", style: TextStyle(fontSize: 24)),
+
+            if (!isLogin.value)
+              ElevatedButton(
+                onPressed: () {
+                  isLogin.value = true;
+                },
+                child: Text("Login"),
+              ),
+            if (isLogin.value)
+              ElevatedButton(
+                onPressed: () {
+                  isLogin.value = false;
+                },
+                child: Text("Logout"),
+              ),
+
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/register');
