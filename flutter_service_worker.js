4b995573d86b8547ae536f508e3d9b3785e2c9bb
Ramy Eldesoky <rdesoky@gmail.com>
2025-12-29 15:09:47 -0800

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c1093e928db692d7ff9c196f392659a0
 "CNAME": "42944dbdc6f9bd05f798c76191446a7b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "7cc097edb4d90b4eb0bf86c3c098dffc",
+"flutter_bootstrap.js": "2bb694e68ccf01dbb17b9c634dc0e761",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
